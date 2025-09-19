c18dd78845ca057877f20e3ab69fef4e11c1d79a
Eds0n08 <galindosuazoedson@gmail.com>
2025-09-19 07:57:05 -0500

    Add files via upload

---
+++ Practicando_ando/src/test/java/com/example/Practicando_ando/PracticandoAndoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Practicando_ando;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticandoAndoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
