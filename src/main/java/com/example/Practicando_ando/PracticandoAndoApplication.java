074857498125dd4139762f52bb6dbb932486a764
Eds0n08 <galindosuazoedson@gmail.com>
2025-09-19 09:16:17 -0500

    V.0.1

---
+++ src/main/java/com/example/Practicando_ando/PracticandoAndoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Practicando_ando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticandoAndoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticandoAndoApplication.class, args);
+	}
+
+}
